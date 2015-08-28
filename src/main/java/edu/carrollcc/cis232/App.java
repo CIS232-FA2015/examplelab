9a0e79b31a31dd9649a97909bf959a26ab0424b3
Dan Rusk <djrusk@gmail.com>
2015-08-27 23:58:12 -0400

    My solution

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,5 +1,7 @@
 package edu.carrollcc.cis232;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+    	Scanner keyboard = new Scanner(System.in);
+        System.out.println(String.format("Hello %s!", keyboard.nextLine()));
     }
 }
