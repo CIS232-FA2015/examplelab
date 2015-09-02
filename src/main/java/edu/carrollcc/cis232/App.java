59279019957589a15ff8a06c57cb21796655fd4e
Dylan Dykes <dzdykes88@gmail.com>
2015-09-01 20:19:29 -0400

    This is my solution for the first lab.
    Signed-off-by: Dylan Dykes <dzdykes88@gmail.com>

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,13 +1,20 @@
 package edu.carrollcc.cis232;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+    	Scanner kb = new Scanner(System.in);
+    	String name = "";
+    	System.out.print("Enter Name: ");
+    	name = kb.nextLine();
+    	System.out.printf("Hello %s!", name);
     }
 }
