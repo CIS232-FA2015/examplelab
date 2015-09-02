14bff1e5882b6b2c106a4a92996cef8f2593348c
Matt VanderVeer <mvande1@umbc.edu>
2015-09-01 20:19:29 -0400

    This is my solution to the lab.
    Signed-off-by: Matt VanderVeer <mvande1@umbc.edu>

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,5 +1,9 @@
 package edu.carrollcc.cis232;
 
+import java.util.Scanner;
+
+
+
 /**
  * Hello world!
  *
@@ -8,6 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+    	System.out.println("Please enter your name.");
+    	Scanner keyboard = new Scanner(System.in);
+    	String name = keyboard.nextLine();
+    	
+        System.out.println("Hello " + name);
     }
 }
