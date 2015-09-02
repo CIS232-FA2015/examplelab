2f6a69d6b569c3f5329a63e421c6647c83b6c5c9
William C. Freeman <myhousemyrules69@yahoo.com>
2015-09-02 17:15:52 -0400

    My practice submission for the lab.

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,5 +1,7 @@
 package edu.carrollcc.cis232;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+    	
+    	Scanner keyboard = new Scanner(System.in);
+    	
+    	String name = "";
+    	
+    	System.out.println("What is your name: ");
+    	
+    	name = keyboard.nextLine();
+    	
+        System.out.printf("Hello %s",name);
     }
 }
